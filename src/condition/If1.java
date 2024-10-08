73bc63ec9a01fc0e396351ac5da6e9f7e2211d39
kwontory <victory.jihyeon@gmail.com>
2024-10-09 00:22:02 +0900

    if

--- src/condition/If1.java
+++ src/condition/If1.java
@@ -2,6 +2,7 @@ package condition;
 
 public class If1 {
     public static void main(String[] args) {
+        // 조건문: 조건에 따라 다른 코드를 실행하고 싶을 때
         int age = 15; // 나이
 
         // if
---
+++ src/condition/Switch1.java
@@ -0,0 +1,7 @@
+package condition;
+
+public class Switch1 {
+    public static void main(String[] args) {
+        // switch..
+    }
+}
