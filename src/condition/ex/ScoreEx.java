370448c84e5d0c04b22470a0b4e238115e35e24c
kwontory <victory.jihyeon@gmail.com>
2024-10-09 21:50:00 +0900

    condition

---
+++ src/condition/ex/ScoreEx.java
@@ -0,0 +1,22 @@
+package condition.ex;
+
+import java.util.Scanner;
+
+public class ScoreEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score =  sc.nextInt();
+
+        if(score >= 90) {
+            System.out.println("A학점");
+        } else if(score >= 80) {
+            System.out.println("B학점");
+        } else if (score >= 70) {
+            System.out.println("C학점");
+        } else if (score >= 60) {
+            System.out.println("D학점");
+        } else {
+            System.out.println("F학점");
+        }
+    }
+}
